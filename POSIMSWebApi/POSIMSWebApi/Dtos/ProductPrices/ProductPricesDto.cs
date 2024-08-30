1378ab54c9772deb92a8f13453f5215318dade3c
keithagreda <104190595+keithagreda@users.noreply.github.com>
2024-08-30 16:56:58 +0800

    product prices services
    product prices services

--- POSIMSWebApi/POSIMSWebApi/Dtos/ProductPrices/ProductPricesDto.cs
+++ POSIMSWebApi/POSIMSWebApi/Dtos/ProductPrices/ProductPricesDto.cs
@@ -1,6 +1,21 @@
-﻿namespace POSIMSWebApi.Dtos.ProductPrices
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using POSIMSWebApi.Dtos.Product;
+
+namespace POSIMSWebApi.Dtos.ProductPrices
 {
     public class ProductPricesDto
     {
+        public Guid Id { get; set; }
+        public decimal Price { get; set; }
+        public DateTime EffectivityDate { get; set; }
+        public string ProductName { get; set; }
+    }
+
+    public class CreateOrEditProductPricesDto
+    {
+        public Guid Id { get; set; }
+        public decimal Price { get; set; }
+        public DateTime EffectivityDate { get; set; }
+        public Guid ProductId { get; set; }
     }
 }
--- POSIMSWebApi/POSIMSWebApi/Services/ProductPricesServices.cs
+++ POSIMSWebApi/POSIMSWebApi/Services/ProductPricesServices.cs
@@ -1,21 +1,40 @@
-﻿using PMSIMSWebApi.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using PMSIMSWebApi.Entities;
 using POSIMSWebApi.Dtos;
 using POSIMSWebApi.Dtos.Product;
 using POSIMSWebApi.Dtos.ProductPrices;
+using POSIMSWebApi.QueryExtensions;
 
 namespace POSIMSWebApi.Services
 {
     public class ProductPricesServices
     {
         private readonly POSIMSDbContext _dbContext;
+
         public ProductPricesServices(POSIMSDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        //public async Task<ApiResponse<IList<ProductPricesDto>>> GetAllProductPrices(FilterText input)
-        //{
-        //    var data = _dbContext.ProductPrices.
-        //}
+        public async Task<ApiResponse<IList<ProductPricesDto>>> GetAllProductPrices(
+            FilterText input
+        )
+        {
+            var data = _dbContext
+                .ProductPrices.Include(e => e.ProductFK)
+                .WhereIf(
+                    !string.IsNullOrWhiteSpace(input.Text),
+                    e => e.ProductFK.Name == input.Text
+                )
+                .Select(e => new ProductPricesDto
+                {
+                    Id = e.Id,
+                    Price = e.Price,
+                    ProductName = e.ProductFK.Name,
+                    EffectivityDate = e.EffectivityDate,
+                });
+
+            if (!await data.AnyAsync()) { }
+        }
     }
 }
